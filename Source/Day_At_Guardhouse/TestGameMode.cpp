801af6bfa10d58e36d34732a3bebb8875269c723
LAPILA <lapila3004@gmail.com>
2024-09-29 23:21:30 +0900

    DBSetting_Complete

--- Source/Day_At_Guardhouse/TestGameMode.cpp
+++ Source/Day_At_Guardhouse/TestGameMode.cpp
@@ -1,5 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TestGameMode.h"
+#include "Kismet/KismetMathLibrary.h"
 
+ATestGameMode::ATestGameMode()
+{
+    PrimaryActorTick.bCanEverTick = false;
+}
+
+void ATestGameMode::StartPlay()
+{
+    Super::StartPlay();
+    ValidateRandomData();
+}
+
+void ATestGameMode::SetValidationProbabilities(float NewErrorProbability, float NewNameProbability, float NewIDProbability, float NewPNProbability, float NewPVProbability, float NewGenderProbability, float NewBirthProbability)
+{
+    ErrorProbability = NewErrorProbability;
+    NameProbability = NewNameProbability;
+    IDProbability = NewIDProbability;
+    PNProbability = NewPNProbability;
+    PVProbability = NewPVProbability;
+    GenderProbability = NewGenderProbability;
+    BirthProbability = NewBirthProbability;
+}
+
+void ATestGameMode::ValidateRandomData()
+{
+    bErrorData = UKismetMathLibrary::RandomBoolWithWeight(ErrorProbability); // 오류 데이터 확률 적용
+
+    // 오류 데이터가 없는 경우 모든 검증 변수를 false로 설정
+    bNameValid = bErrorData ? UKismetMathLibrary::RandomBoolWithWeight(NameProbability) : false;
+    bIDValid = bErrorData ? UKismetMathLibrary::RandomBoolWithWeight(IDProbability) : false;
+    bPNValid = bErrorData ? UKismetMathLibrary::RandomBoolWithWeight(PNProbability) : false;
+    bPVValid = bErrorData ? UKismetMathLibrary::RandomBoolWithWeight(PVProbability) : false;
+    bGenderValid = bErrorData ? UKismetMathLibrary::RandomBoolWithWeight(GenderProbability) : false;
+    bBirthValid = bErrorData ? UKismetMathLibrary::RandomBoolWithWeight(BirthProbability) : false;
+}
+
+void ATestGameMode::DebugPrintValidationResults()
+{
+    // 디버그 정보 출력
+    GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Debug Information: ErrorData=%s, NameValid=%s, IDValid=%s, PNValid=%s, PVValid=%s, GenderValid=%s, BirthValid=%s"),
+        bErrorData ? TEXT("True") : TEXT("False"),
+        bNameValid ? TEXT("True") : TEXT("False"),
+        bIDValid ? TEXT("True") : TEXT("False"),
+        bPNValid ? TEXT("True") : TEXT("False"),
+        bPVValid ? TEXT("True") : TEXT("False"),
+        bGenderValid ? TEXT("True") : TEXT("False"),
+        bBirthValid ? TEXT("True") : TEXT("False")));
+}
--- Source/Day_At_Guardhouse/TestGameMode.h
+++ Source/Day_At_Guardhouse/TestGameMode.h
@@ -12,6 +12,50 @@
 UCLASS()
 class DAY_AT_GUARDHOUSE_API ATestGameMode : public AGameModeBase
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    ATestGameMode();
+
+    virtual void StartPlay() override;
+
+    // 데이터 유효성 검증 함수
+    UFUNCTION(BlueprintCallable, Category = "Data Validation")
+    void ValidateRandomData();
+
+    // 데이터 확률 조정 함수
+    UFUNCTION(BlueprintCallable, Category = "Data Validation")
+    void SetValidationProbabilities(float NewErrorProbability, float NewNameProbability, float NewIDProbability, float NewPNProbability, float NewPVProbability, float NewGenderProbability, float NewBirthProbability);
+
+    // 디버깅 정보 출력 함수
+    UFUNCTION(BlueprintCallable, Category = "Debug")
+    void DebugPrintValidationResults();
+
+    /** 오류 데이터 존재 여부 */
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bErrorData;
+
+    /** 각 데이터 유효성 변수 */
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bNameValid;
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bIDValid;
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bPNValid;
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bPVValid;
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bGenderValid;
+    UPROPERTY(BlueprintReadOnly, Category = "Random Data")
+    bool bBirthValid;
+
+private:
+    // 확률 변수들
+    float ErrorProbability;
+    float NameProbability;
+    float IDProbability;
+    float PNProbability;
+    float PVProbability;
+    float GenderProbability;
+    float BirthProbability;
 };
