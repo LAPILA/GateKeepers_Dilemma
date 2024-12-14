c671bb2c9e0f938e78b722aab8fd64f903913057
dureamPark <doo7172@naver.com>
2024-12-14 23:16:04 +0900

    System Complete
    전반적인 시스템 완성. 인트로 아웃트로 및 조사할 때 데이터테이블 내용 가져오는거만 하면 끝.

--- Intermediate/Build/BuildRulesProjects/Day_At_GuardhouseModuleRules/obj/Development/Day_At_GuardhouseModuleRules.AssemblyInfo.cs
+++ Intermediate/Build/BuildRulesProjects/Day_At_GuardhouseModuleRules/obj/Development/Day_At_GuardhouseModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day_At_GuardhouseModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940226cbb2a97f039c0e71f2bced81800ffa85fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15584be3d0dbe19e984fb76ff614946bc3c9ae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day_At_GuardhouseModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day_At_GuardhouseModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
